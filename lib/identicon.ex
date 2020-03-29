e3c14084ec05d47f14a3ec3f63d7e45cfdd57ef1
pokkew <brunogabrielpk@gmail.com>
2020-03-28 20:01:38 -0400

    Versão inicial finalizada

---
+++ lib/identicon.ex
@@ -0,0 +1,87 @@
+defmodule Identicon do
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save(input)
+  end
+
+  def save(image,input) do
+    #binary = :erlang.term_to_binary(image)
+    File.write("#{input}.png",image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250,250)
+    fill = :egd.color(color)   
+
+    Enum.each(pixel_map, fn({start, stop}) ->
+       :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(image) do
+    pixel_map = Enum.map image.grid, fn({_code, index}) ->
+      horizontal = rem(index,5) * 50
+      vertical = div(index,5) * 50
+      
+      top_left = {horizontal,vertical}
+      bottom_right = {horizontal+50,vertical+50}
+
+      {top_left,bottom_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map} 
+  end
+
+  def filter_odd_squares(image) do
+    grid = Enum.filter image.grid, fn({code, _index})-> 
+      rem(code,2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def build_grid(image) do
+    grid = 
+      image.hex
+      |> Enum.chunk(3) # chunk deprecaded but still works
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first,second | _tail] = row
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  # js equivalent
+  # pick_color: function(image) {
+  #   image.color = {
+  #     r: image.hex[0],
+  #     g: image.hex[1],
+  #     b: image.hex[2]
+  #   };
+  #   return image
+  # }
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
